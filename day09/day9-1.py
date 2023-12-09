f8aa6b6fbc41ad052aea5102590194f00d18e9f6
Steve Calder <steve.r.calder@gmail.com>
2023-12-09 09:18:06 -0500

    why was part 2 so easy all of a sudden?

---
+++ day09/day9-1.py
@@ -0,0 +1,27 @@
+files = open('day09/day9data.txt', 'r')
+lines = files.readlines()
+
+def calculate_next(values):
+  diffs = []
+
+  for i in range(1, len(values)):
+    diff = values[i] - values[i-1]
+    diffs.append(diff)
+
+  if all(d == 0 for d in diffs):
+    return values[-1]
+
+  next_value = calculate_next(diffs)
+
+  return values[-1] + next_value
+
+total = 0
+
+for line in lines:
+  line = line.strip()
+  values = line.split(' ')
+  values = [int(v) for v in values]
+
+  total = total + calculate_next(values)
+
+print(total)
