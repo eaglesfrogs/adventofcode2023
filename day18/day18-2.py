01603705a83daf3fdc2879aab655ba065a348dd5
Steve Calder <steve.r.calder@gmail.com>
2023-12-18 17:31:50 -0500

    done 18

--- day18/day18-2.py
+++ day18/day18-2.py
@@ -1,7 +1,11 @@
+import shapely
+
 files = open('day18/day18data.txt', 'r')
 lines = files.readlines()
 
 directions = []
+
+perimeter = 0
 
 for line in lines:
     line = line.strip()
@@ -20,68 +24,37 @@ for line in lines:
 
     steps = int(hex[0:5], 16)
 
+    perimeter += steps
+
     directions.append((dir, steps))
 
-lagoon_hash = {}
+lagoon_list = []
 
-next_space = (0, 0)
+current_space = (0, 0)
+min_x = 0
+min_y = 0
 
 for dir in directions:
-    for i in range(dir[1]):
-        lagoon_hash[next_space] = dir[2]
+    next_space = None
 
-        if dir[0] == 'R':
-            next_space = (next_space[0], next_space[1] + 1)
-            if next_space[1] > max_x:
-                max_x = next_space[1]
-        elif dir[0] == 'L':
-            next_space = (next_space[0], next_space[1] - 1)
-            if next_space[1] < min_x:
-                min_x = next_space[1]
-        elif dir[0] == 'U':
-            next_space = (next_space[0] - 1, next_space[1])
-            if next_space[0] < min_y:
-                min_y = next_space[0]
-        elif dir[0] == 'D':
-            next_space = (next_space[0] + 1, next_space[1])
-            if next_space[0] > max_y:
-                max_y = next_space[0]
+    if dir[0] == 'R':
+        next_space = (current_space[0], current_space[1] + dir[1])
+    elif dir[0] == 'L':
+        next_space = (current_space[0], current_space[1] - dir[1])
+    elif dir[0] == 'U':
+        next_space = (current_space[0] - dir[1], current_space[1])
+    elif dir[0] == 'D':
+        next_space = (current_space[0] + dir[1], current_space[1])
 
-lagoon_array = []
+    lagoon_list.append(next_space)
+    current_space = next_space
 
-row_count = abs(min_y) + abs(max_y) + 3
-col_count = abs(min_x) + abs(max_x) + 3
+# knew i had to figure out the area, found a library to calculate the area instead of implement shoelace formula
+# https://stackoverflow.com/questions/24467972/calculate-area-of-polygon-given-x-y-coordinates
+polygon = shapely.Polygon(lagoon_list)
 
-# plus three to add a border around the whole thing for an exterior flood fill
-for i in range(row_count):
-    lagoon_array_row = []
-    for j in range(col_count):
-        lagoon_array_row.append('.')
-    lagoon_array.append(lagoon_array_row)
+# like a dummy i forgot that i gotta count for the area of the border
+# https://www.reddit.com/r/adventofcode/comments/18l2tap/comment/kdv8imu/?utm_source=share&utm_medium=web2x&context=3
+area = polygon.area + (perimeter/2) + 1
 
-# plus 1 on coordinates to account for floodfill border
-for l in lagoon_hash:
-    lagoon_array[l[0] + abs(min_y) + 1][l[1] + abs(min_x) + 1] = '#'
-
-flood_fill_stack = [(0, 0)]
-
-while flood_fill_stack:
-    f = flood_fill_stack.pop()
-    lagoon_array[f[0]][f[1]] = 'X'
-
-    if f[0] > 0 and lagoon_array[f[0] - 1][f[1]] == '.':
-        flood_fill_stack.append((f[0] - 1, f[1]))
-    if f[0] < row_count - 1 and lagoon_array[f[0] + 1][f[1]] == '.':
-        flood_fill_stack.append((f[0] + 1, f[1]))
-    if f[1] > 0 and lagoon_array[f[0]][f[1] - 1] == '.':
-        flood_fill_stack.append((f[0], f[1] - 1))
-    if f[1] < col_count - 1 and lagoon_array[f[0]][f[1] + 1] == '.':
-        flood_fill_stack.append((f[0], f[1] + 1))
-
-total = 0
-for row in lagoon_array:
-    for c in row:
-        if c == '#' or c == '.':
-            total += 1
-
-print(total)
+print(area)
